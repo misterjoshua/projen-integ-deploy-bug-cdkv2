4a8fd44d8a5d4a98255a9ac76d6b4d832d701024
Josh Kellendonk <joshkellendonk@gmail.com>
2021-12-14 16:51:38 -0700

    chore: a few more cleanups

--- .projenrc.js
+++ .projenrc.js
@@ -1,11 +1,13 @@
-const { awscdk } = require('projen');
-const project = new awscdk.AwsCdkConstructLibrary({
+const pj = require('projen');
+const project = new pj.awscdk.AwsCdkConstructLibrary({
   author: 'Josh Kellendonk',
   authorAddress: 'joshkellendonk@gmail.com',
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
   name: 'test-a',
   repositoryUrl: 'https://github.com/joshkellendonk/test-a.git',
+
+  releaseTrigger: pj.release.ReleaseTrigger.manual(),
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
