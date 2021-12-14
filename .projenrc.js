5a5cafbcbb84bbb283f7fe2ba0a9e07f7314a58b
Josh Kellendonk <joshkellendonk@gmail.com>
2021-12-14 16:28:13 -0700

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,16 @@
+const { awscdk } = require('projen');
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Josh Kellendonk',
+  authorAddress: 'joshkellendonk@gmail.com',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  name: 'test-a',
+  repositoryUrl: 'https://github.com/joshkellendonk/test-a.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+  // release: undefined,      /* Add release management to this project. */
+});
+project.synth();
